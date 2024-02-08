b3f84d050b1f173f6676c4e662c3f882523d6b9a
Tristan <tristanchay123@gmail.com>
2024-02-08 21:25:22 +0800

    Initial Commit

---
+++ BoxSort/BoxSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxSortApp.swift
+//  BoxSort
+//
+//  Created by Tristan Chay on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BoxSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
