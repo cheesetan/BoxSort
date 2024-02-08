1e7ab80642ccc9d1478d10ca4036bc9230883aa8
Tristan <tristanchay123@gmail.com>
2024-02-08 21:26:42 +0800

    sorted files

---
+++ BoxSort/Application/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  BoxSort
+//
+//  Created by Tristan Chay on 8/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        BoxesView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ BoxSort/Main/UI/BoxesView.swift
@@ -0,0 +1,22 @@
+//
+//  BoxesView.swift
+//  BoxSort
+//
+//  Created by Tristan Chay on 8/2/24.
+//
+
+import SwiftUI
+
+struct BoxesView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    BoxesView()
+}
